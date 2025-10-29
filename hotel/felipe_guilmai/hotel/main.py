47bf36890e6af7a644982352dd055b30547685c9
felipe_guilmai <felipe.guilmai@inacapmail.cl>
2025-10-29 20:33:03 -0300

    felipe

---
+++ hotel/felipe_guilmai/hotel/main.py
@@ -0,0 +1,7 @@
+from hotel.felipe_guilmai.hotel.personas import personalAseo, manager, cliente, chef, recepcionista
+from hotel.felipe_guilmai.hotel.objetos import comida, habitacion, invetario, boleta
+
+def __main__():
+    print("aplicacion ejecutandose")
+
+def controlinventario(): 
