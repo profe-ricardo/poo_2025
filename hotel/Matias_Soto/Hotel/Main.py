63f0b22bca77893584e2282430aa51636de47db1
Happy <Mati.chino20@gmail.com>
2025-10-29 20:30:47 -0300

    agrego

--- hotel/Matias_Soto/Hotel/Main.py
+++ hotel/Matias_Soto/Hotel/Main.py
@@ -1,3 +1,13 @@
-from hotel.Matias_Soto.Personas import Personal_Aseo, Manager, Clientes, Recepcionista
-from hotel.Matias_Soto.Objetos import Habitacion,Comida,Inventario, Boleta
+from Personas import Personal_Aseo, Manager, Clientes, Recepcionista
+from Objetos import Habitacion,Comida, Inventario, Boleta 
 
+def _main_():
+    print("Aplicacion ejecutandose")
+
+    bebida = Inventario("gaseosa", "Restock")
+    controlInventario(bebida)
+
+def controlInventario(obj: Inventario):
+    obj.revisarInventario()
+
+_main_()
