32490c09bbdcab2b1a030f0afa244bc75acde88b
Luis-Vicencio <luis.vicencio8@gmail.com>
2025-11-03 21:13:10 -0300

    avance

--- hotel/Luis_vicencio/main.py
+++ hotel/Luis_vicencio/main.py
@@ -1,3 +1,13 @@
-from personas import personalAseo, manager, cliente, chef, recepcionista
-from objetos import comida, habitacion,inventarios, boleta
+from view.personas import chef
 
+def __main__():
+    print("aplicacion iniciada")
+    chefcito = chef("Remi", 1, "paris", ["Ratatoullie"])
+
+    print(chefcito.ver_pedidos())
+
+    print(chefcito.recibir_pedido(["Bebida", "Completo", "Pie de limon"]))
+
+    print(chefcito.ver_pedidos())
+
+__main__()
