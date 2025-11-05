5429976959b8390908f7b52ffcbd07c7f42bfbd0
GhostzZ21 <f.herrera.c2002@gmail.com>
2025-11-05 20:34:18 -0300

    NIGGEEEEEEEEEEEEEEER

--- hotel/Francisco_Herrera/main.py
+++ hotel/Francisco_Herrera/main.py
@@ -1,18 +1,35 @@
 # from hotel.Francisco_Herrera.personas import personalAseo, manager, cliente, chef, recepcionista
 # from hotel.Francisco_Herrera.objetos import comida, habitacion, inventario, boleta
 from view.personas import chef
+from config.db_config import ConexionOracle
 
-def __main__():
+def conectarBD():
+    db = ConexionOracle("system","Ina.2025","localhost:1521/xe")
+    db.conectar()
+
+    return db
+
+def main():
+    db = conectarBD()
+
     print("Aplicacion Iniciada")
-    chefcito = chef ("Remi", 1, "Paris", ["Ratatoullie"])
 
-    print(chefcito.ver_pedidos())
+    db.desconectar()  
 
-    print(chefcito.procesar_pedido(["Bebida", "Completo", "Pie de Limon"]))
+if __name__ == "__main__":
+    main()
 
-    print(chefcito.ver_pedidos())
+# def __main__():
+#     print("Aplicacion Iniciada")
+#     chefcito = chef ("Remi", 1, "Paris", ["Ratatoullie"])
 
-__main__()
+#     print(chefcito.ver_pedidos())
+
+#     print(chefcito.procesar_pedido(["Bebida", "Completo", "Pie de Limon"]))
+
+#     print(chefcito.ver_pedidos())
+
+# __main__()
 
 #    bebida = inventario("Gaseosa","Recien llegada")
 
