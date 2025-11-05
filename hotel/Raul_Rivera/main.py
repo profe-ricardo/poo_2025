bfef8fa9f5b2821c266e2f5c13990eb09e234101
1R4VL <raul.rivera10@inacapmail.cl>
2025-11-05 20:23:48 -0300

    Coneccion Oracle

--- hotel/Raul_Rivera/main.py
+++ hotel/Raul_Rivera/main.py
@@ -1,13 +1,27 @@
-from view.personas import chef
+from config.db_config import ConexionOracle
+# from view.personas_v import chef
 
-def __main__():
-    print('Aplicacion iniciada')
-    chefcito = chef('Remi',1,'Paris',['Ratatoullie'])
+def conectarDB():
+    db=ConexionOracle('system','Ina.2025','localhost:1521/xe')
+    db.conectar()
 
-    print(chefcito.ver_pedidos())
+    return db
 
-    print(chefcito.recibir_pedido(['Bebida','Completo', 'Pie de limon']))
+def main():
+    db= conectarDB()
 
-    print(chefcito.ver_pedidos())
+    print('Aplicacion Iniciada')
 
-__main__()
+    db.desconectar()
+
+# def __main__():
+#     print('Aplicacion iniciada')
+#     chefcito = chef('Remi',1,'Paris',['Ratatoullie'])
+
+#     print(chefcito.ver_pedidos())
+
+#     print(chefcito.recibir_pedido(['Bebida','Completo', 'Pie de limon']))
+
+#     print(chefcito.ver_pedidos())
+
+main()
