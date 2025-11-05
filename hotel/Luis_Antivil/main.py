611af2e5c5d6ed17a5ed895bfc44178564ff2d79
Curantovish <luisjkantivil@gmail.com>
2025-11-05 20:33:30 -0300

    actualizacion

--- hotel/Luis_Antivil/main.py
+++ hotel/Luis_Antivil/main.py
@@ -1,13 +1,32 @@
-from view.personas import chef
+# from view.personas import chef
 
-def __main__(): 
-    print("Aplicacion iniciada")
-    chefcito= chef('Remi', 1, 'Paris', ['Ratatoullie'])
+# def __main__(): 
+#     print("Aplicacion iniciada")
+#     chefcito= chef('Remi', 1, 'Paris', ['Ratatoullie'])
 
-    print(chefcito.ver_pedidos())
+#     print(chefcito.ver_pedidos())
 
-    print(chefcito.recibir_pedidos(['hola']))
+#     print(chefcito.recibir_pedidos(['hola']))
 
-    print(chefcito.ver_pedidos())
+#     print(chefcito.ver_pedidos())
 
-__main__()
+# __main__()
+
+from config.db_config import ConexionOracle
+
+
+def conectarBD():
+    db=ConexionOracle("system","Ina.2025","localhost:1521/xe")
+    db.conectar()
+
+    return db
+
+def main():
+    db=conectarBD()
+
+    print("aplicacion iniciada")
+
+    db.desconectar()
+    
+if __name__=="__main__":
+    main()
