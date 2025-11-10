f062f34f7ef3550d59a7cb48583810788c521ac6
christianav-22 <chrastian2021@gmail.com>
2025-11-10 20:30:12 -0300

    avance

--- hotel/Chr_vargas/main.py
+++ hotel/Chr_vargas/main.py
@@ -1,3 +1,20 @@
-from personas import personalAseo, manager, cliente, chef, recepcionista
-from objetos import comida, habitacion,inventarios, boleta
+# from personas import personalAseo, manager, cliente, chef, recepcionista
+# from objetos import comida, habitacion,inventarios, boleta
 
+from config.db_config import ConexionOracle
+
+def conectarBD():
+    db = ConexionOracle("system", "Ina.2025", "localhost:1521/xe")
+    db.conectar() 
+
+    return db
+
+def main():
+    db = conectarBD()
+
+    print("Aplicacion iniciada")
+
+    db.desconectar()
+
+if __name__ == "__main__":
+    main()
--- hotel/Chr_vargas/view/main.py
+++ hotel/Chr_vargas/view/main.py
@@ -1,13 +1,31 @@
-from view.personas import chef
+# from hotel.Chr_vargas.view.personas_v import chef
 
-def __main__():
+# def __main__():
+#     print("Aplicacion iniciada")
+#     chefcito = chef("remi", 1, "Paris", ["Ratatouille"])
+
+#     print(chefcito.ver_pedidos())
+
+#     print(chefcito.recibir_pedidos(['Bebida', 'Completo', 'Pie de limon']))
+
+#     print(chefcito.ver_pedidos())
+
+# __main__()
+
+from config.db_config import ConexionOracle
+
+def conectarBD():
+    db = ConexionOracle("system", "Ina.2025", "localhost:1521/xe")
+    db.conectar() 
+
+    return db
+
+def main():
+    db = conectarBD()
+
     print("Aplicacion iniciada")
-    chefcito = chef("remi", 1, "Paris", ["Ratatouille"])
 
-    print(chefcito.ver_pedidos())
+    db.desconectar()
 
-    print(chefcito.recibir_pedidos(['Bebida', 'Completo', 'Pie de limon']))
-
-    print(chefcito.ver_pedidos())
-
-__main__()
+if __name__ == "__main__":
+    main()
