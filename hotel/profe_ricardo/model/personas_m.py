df350f45922b875b37d1a6cbde142ff51dbcc5ea
profe-ricardo <ricardo.gutierrez22@inacapmail.cl>
2025-11-10 21:10:41 -0300

    [feat] inventario poo

--- hotel/profe_ricardo/model/personas_m.py
+++ hotel/profe_ricardo/model/personas_m.py
@@ -1,19 +1,3 @@
-# class chef:
-#     def __init__(self, nombre: str, id: int, locacion: str, pedidos: list):
-#         self.nombre = nombre
-#         self.id = id
-#         self.locacion = locacion
-#         self.pedidos = pedidos
-
-#     def tomar_pedidos(self, pedido: list) -> bool:
-#         for p in pedido:
-#             self.pedidos.append(p)
-
-#         return True
-    
-#     def ver_pedidos(self) -> list:
-#         return self.pedidos
-
 from hotel.profe_ricardo.config.db_config import ConexionOracle
 
 class UsuarioModel:
