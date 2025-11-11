7805f0f99e40128f9c83a36cdc66d199953929f4
Curantovish <luisjkantivil@gmail.com>
2025-11-11 20:59:03 -0300

    no se que paso pero veamos

---
+++ hotel/Luis_Antivil/Acumulativa3/model/Recepcionista.py
@@ -0,0 +1,11 @@
+from Usuario import UsuarioModel
+from hotel.Luis_Antivil.config.db_config import ConexionOracle
+class recepcionista(): 
+    def __init__(self, nombre:str, telefono:int, ubicacion:str, conexion:ConexionOracle):
+        super().__init__(nombre,telefono,ubicacion, conexion)
+        self.nombre=nombre
+        self.telefono=telefono
+        self.ubicacion=ubicacion
+        self.conexion=conexion
+    
+    
