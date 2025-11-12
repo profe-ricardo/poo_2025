bc43df2f188a008223b150c984924ca3fd9ba019
Curantovish <luisjkantivil@gmail.com>
2025-11-11 21:04:17 -0300

    funciono XD

--- hotel/Luis_Antivil/Acumulativa3/model/Recepcionista.py
+++ hotel/Luis_Antivil/Acumulativa3/model/Recepcionista.py
@@ -1,6 +1,10 @@
 from Usuario import UsuarioModel
 from hotel.Luis_Antivil.config.db_config import ConexionOracle
 class recepcionista(): 
+    """
+    se debe poder crear, modificar, eliminar y leer \n
+    los datos de la cuenta del recepcionista
+    """
     def __init__(self, nombre:str, telefono:int, ubicacion:str, conexion:ConexionOracle):
         super().__init__(nombre,telefono,ubicacion, conexion)
         self.nombre=nombre
@@ -8,6 +12,6 @@ class recepcionista():
         self.ubicacion=ubicacion
         self.conexion=conexion
         
-        
+    
     
     
