205e06d3116a02e0bdb308da91bc189171dee025
tamaravera <tamarav027@gmail.com>
2025-11-12 18:58:33 -0300

    avance

---
+++ hotel/tamara/Acumulativa3/controller/objetos.py
@@ -0,0 +1,24 @@
+import re 
+from hotel.tamara.model.objetos_m import InventarioModel
+
+sus_keys = [
+    r";", r"--", r"/\*", r"\bOR\b", r"\bAND\b", r"\bUNION\b",
+    r"\bSELECT\b", r"\bINSERT\b", r"\bUPDATE\b", r"\bDELETE\b",
+    r"\bDROP\b", r"\bEXEC\b"
+]
+
+patron = re.compile("|".join(sus_keys), re.IGNORECASE)
+
+class InventarioController:
+    def __init__(self, modelo: InventarioModel):
+        self.modelo = modelo
+
+    def registrar_inventario(self, nombre: str, tipo: str, cantidad: int, precio_costo: float):
+
+        if patron.search(nombre) or patron.search(tipo):
+            print("[ERROR]: No se puede ingresar código SQL en los string.")
+
+            return False
+        
+        else:
+            return self.modelo.guardar_item(nombre, tipo, cantidad, precio_costo)
---
+++ hotel/tamara/Acumulativa3/model/personas_m.py
@@ -0,0 +1,7 @@
+class cliente():
+    def __main__(self, nombre: str, telefono: int, nacionalidad: str, habitacion: int):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.nacionalidad = nacionalidad
+        self.habitacion = habitacion
+
