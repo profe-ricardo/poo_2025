f9152ed3d53a6027c48daa00b56ca372ae5ab177
Luis-Vicencio <luis.vicencio8@gmail.com>
2025-11-17 21:22:31 -0300

    ultima Clase

--- hotel/Luis_vicencio/main.py
+++ hotel/Luis_vicencio/main.py
@@ -12,16 +12,21 @@
 
 # main()
 
-# from config.db_config import ConexionOracle
+import bcrypt
+
+from config.db_config import ConexionOracle
 # #from model.personas import UsuarioModel
 # #from controller.personas import UsuarioController
 # #from view.personas import UsuarioView
 
-# def conectarBD():
-#     db = ConexionOracle("system", "vicencio", "localhost:1521/xe")
-#     db.conectar()
+def conectarBD():
 
-#     return db
+    db = ConexionOracle("system", "Ina.2025", "localhost:1521/xe")
+    db.conectar()
+    
+    # validar_tablas(db)
+
+    return db
 
 # def main():
 #     db = conectarBD()
@@ -29,14 +34,31 @@
 #     print("Aplicacion iniciada")
 
 #     db.desconectar()
-    
-# if __name__ == "__main__":
-#     main()
 
-from model.objetos import ver_inventario  
-  
+
+#from model.objetos import ver_inventario
+
 def main():
-    print("aplicacion iniciada")
+    db = conectarBD()
+    cursor = db.obtener_cursor()
+    print("Inicio de sesion, Ingrese sus credenciales\n")
+    id_u = int(input("ingrese su ID: "))
+    usuario = str(input("ingrese su nombre de usuario: "))
+    clave = str(input("ingrese su Clave: "))
+    clave = bytes(clave, encoding="utf-8")
+
+    salt = bcrypt.gensalt()
+    clave_encriptada = bcrypt.hashpw(clave, salt)
+
     
+
+    consulta = "insert into usuarios (id, nombre_usuario, clave) values (:1, :2, :3)"
+    cursor.execute(consulta, (id_u, usuario, clave_encriptada))
+    db.connection.commit()
     
-    
+    db.desconectar()
+
+if __name__ == "__main__":
+    main()
+
+
