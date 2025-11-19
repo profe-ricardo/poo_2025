c43c794ab72e889e6f96ac370a844309038cd6d8
Luis-Vicencio <luis.vicencio8@gmail.com>
2025-11-19 20:32:52 -0300

    ultima clase

--- hotel/Luis_vicencio/main.py
+++ hotel/Luis_vicencio/main.py
@@ -42,20 +42,40 @@ def main():
     db = conectarBD()
     
     print("Inicio de sesion, Ingrese sus credenciales\n")
-    id_u = int(input("ingrese su ID: "))
+    
     usuario = str(input("ingrese su nombre de usuario: "))
     clave = str(input("ingrese su Clave: "))
     clave = bytes(clave, encoding="utf-8")
 
     salt = bcrypt.gensalt()
     clave_encriptada = bcrypt.hashpw(clave, salt)
+    clave_encriptada = clave_encriptada.decode("utf-8")
 
     cursor = db.obtener_cursor()
 
-    consulta = "insert into usuarios (id, nombre_usuario, clave) values (:1, :2, :3)"
-    cursor.execute(consulta, (id_u, usuario, clave_encriptada))
+    consulta = "insert into usuarios ( nombre_usuario, clave) values (:1, :2)"
+    cursor.execute(consulta, ( usuario, clave_encriptada,))
+
     db.connection.commit()
     
+    usuario = str(input("ingrese su nombre de usuario: "))
+    clave = str(input("ingrese su Clave: "))
+
+    consulta = "select clave from usuarios where nombre_usuario = :1"
+    clave_data = cursor.execute(consulta, (usuario,))
+    clave_db = cursor.fetchone()
+    clave_bytes = bytes(clave, encoding="utf-8")
+    clave_test = bytes(clave_db[0], encoding="utf-8")
+    
+
+    validacion_clave = bcrypt.checkpw(clave_bytes, clave_test)
+
+    if validacion_clave:
+        print("ingreso Correcto")
+    else:
+        print("credenciales Incorrectas")
+
+
     db.desconectar()
 
 if __name__ == "__main__":
