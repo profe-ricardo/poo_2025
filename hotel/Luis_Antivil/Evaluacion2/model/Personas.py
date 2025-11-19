27c142e096cd8c91bf21ca6bf535ce874a63d925
Curantovish <luisjkantivil@gmail.com>
2025-11-19 01:36:43 -0300

    avance

---
+++ hotel/Luis_Antivil/Evaluacion2/model/Personas.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from Usuarios import UsuarioModel
+
+class Paciente(UsuarioModel):
+    def __init__(self, comuna:str, fecha_primera_visita:datetime)
+    super().__init__(id, nombre_usuario)
+    """"""
+    
+class Medico():
+    def __init__(self, especialidad:str, horario_atención:str, fecha_ingreso:datetime)
+    
+    
+    """"""
+    
+    
